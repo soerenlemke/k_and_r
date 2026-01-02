ef988181dece300130e2e53df99f4aee40158285
soerenlemke <61249756+soerenlemke@users.noreply.github.com>
2026-01-02 23:54:52 +0100

    exercise 1.8 done

---
+++ chap01/ex01_08.c
@@ -0,0 +1,32 @@
+//
+// Created by soerenlemke on 02.01.26.
+//
+
+#include <stdio.h>
+
+// testable by running and then STRG + D for ending console
+
+int main(void) {
+    int character;
+    int newline_counter = 0;
+    int tab_counter = 0;
+    int blank_counter = 0;
+
+    while ((character = getchar()) != EOF) {
+        if (character == '\n') {
+            newline_counter++;
+        }
+        if (character == '\t') {
+            tab_counter++;
+        }
+        if (character == ' ') {
+            blank_counter++;
+        }
+    }
+
+    printf("Newline counter: %d\n", newline_counter);
+    printf("Tab counter: %d\n", tab_counter);
+    printf("Blank counter: %d\n", blank_counter);
+
+    return 0;
+}
